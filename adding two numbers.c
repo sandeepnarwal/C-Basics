dc6c5b9b1b35a5955e8e3ab84a6ac5d5472aa340
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-10-20 12:16:01 +0530

    Add files via upload

---
+++ adding two numbers.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	int number1=234,number2=345;
+	printf("addition of %d and %d is %d",number1,number2,number1+number2);
+}
---
+++ student information.c
@@ -0,0 +1,17 @@
+/* c programm to print int,float,char,*/
+#include<stdio.h>
+void main()
+{
+	//variable Declaration
+	int Age;
+	float Cgpa;
+	char initial;
+	//initialization
+	Age= 21;
+	Cgpa= 8.0;
+	initial= 'T';
+	//output
+	printf("age of the student is: %d\n",Age);
+	printf("cgpa of the student is: %f\n",Cgpa);
+	printf("student initial start with: %c",initial);
+}
