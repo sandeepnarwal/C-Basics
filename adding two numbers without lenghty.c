16159addeac3a203a8587dfffedec19c8455c4a0
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-10-20 12:18:28 +0530

    Add files via upload

---
+++ adding two numbers without lenghty.c
@@ -0,0 +1,17 @@
+/* c programm to print int,float,char,*/
+#include<stdio.h>
+void main()
+{
+	//variable Declaration
+	int age;
+	float Cgpa;
+	char initial;
+	//initialization
+	age=21;
+	Cgpa=9.3;
+	initial='T';
+	//output
+	printf("age of the student is %d");
+	printf("cgpa of the student is %f");
+	printf("student initial start with %c");
+}
