9325b206b35817d0fe996ade2cb3dbbef58004ea
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-10-22 19:39:12 +0530

    Update student marks.c

--- student marks.c
+++ student marks.c
@@ -13,7 +13,7 @@ int main()
 	//output
 	printf("student uptained marks in maths:%d\n",maths);
 	printf("student uptained marks in science:%d\n",science);
-    printf("student uptained marks in chemistry:%d\n",chemistry);
+        printf("student uptained marks in chemistry:%d\n",chemistry);
 	printf("student uptained marks in english:%d\n",maths);
 	printf("student total marks:%d\n",maths+science+chemistry+english);
 	
