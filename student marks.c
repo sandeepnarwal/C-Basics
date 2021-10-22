22966e7e5a44ee9124fa8cf4c5429c89f151136d
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-10-22 19:38:28 +0530

    Add files via upload

---
+++ student marks.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	//variable diclarization
+	int maths,science,chemistry,english;
+	
+	//initilazion
+	maths=88;
+	science=96;
+	chemistry=85;
+	english=93;
+	maths+science+chemistry+english;
+	//output
+	printf("student uptained marks in maths:%d\n",maths);
+	printf("student uptained marks in science:%d\n",science);
+    printf("student uptained marks in chemistry:%d\n",chemistry);
+	printf("student uptained marks in english:%d\n",maths);
+	printf("student total marks:%d\n",maths+science+chemistry+english);
+	
+}
