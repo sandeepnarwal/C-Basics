cf58f7fe397a4439288bb9b37da27ab434ff9706
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-11-03 12:23:36 +0530

    Add files via upload

---
+++ conversion of min into hrs and min.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{ int min=100,hr=min/60,x=min%60;
+printf("%d hours %d minutes",hr,x);
+	}
+	//alternate
+	//int min;
+   //min=100;
+   //printf("%d hours %d minutes",hr/min,min%60);
+	
