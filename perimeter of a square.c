16fc07946c740160a395f68ec10620f01b9b7d22
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-11-03 12:27:29 +0530

    Add files via upload

---
+++ perimeter of a square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+     int n,area,perimeter;
+     n=4;
+    
+     area=n*n;
+     perimeter=4*n;
+     
+     printf("area of the square with side:%d\n",area);
+     printf("perimeter of the square with side:%d\n",perimeter);
+}
