fd64f9fb0d8328e8948bfc38d869b67468607381
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-11-09 11:55:35 +0530

    Add files via upload

---
+++ scanf example.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter four numbers :");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("number 1:%d\n", a);
+	printf("number 2:%d\n", b);
+	printf("number 3:%d\n", c);
+	printf("number 4:%d\n", d);
+    printf(" product of a, b, c and d is:%d",a*b*c*d);
+
+}
