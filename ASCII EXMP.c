2ed8df332b1a8672279f7e5f1b51906bd9a157c9
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-11-09 12:27:24 +0530

    Add files via upload

---
+++ ASCII EXMP.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	 char n;
+    printf(" enter a charater (a-z or A-Z):");
+    scanf("%ch",&n);
+    printf("corresponding ASCII charater for %d is %c",n,n);
+ 
+ }
+
