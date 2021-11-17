5a50047087cd2e54b6c7aae34913449f49050fc9
sandeepnarwal <92290542+sandeepnarwal@users.noreply.github.com>
2021-11-17 12:14:29 +0530

    Add files via upload

---
+++ assign operators p2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c;
+	a = 10;
+	b = 4;
+	c = 3;
+	a += b;//14 4 3
+	b -= c;//14 1 3
+	c *= a;//14 1 42
+	a /= b;//14 1 42
+	b *= c;//14 42 42
+	a += b;//56 42 42
+	
+	printf("%d %d %d", b , a ,c);
+}
